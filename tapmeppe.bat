913c397b6a30968e3dc2557f8040a7da793fa26a
Patrick Meppe <patrick.meppe@tapmeppe.work>
2026-04-16 21:10:36 +0200

    refactoring: Added a command to the CLI.

---
+++ tapmeppe.bat
@@ -0,0 +1,43 @@
+@echo off
+@REM @since PM (13.05.2024) This script is used to run convinient processes.
+@REM testing
+@REM WIN + R ++ cmd OR WIN ++ cmd
+@REM ...
+cls
+@REM goto :eof
+
+@REM color output
+@REM @link https://stackoverflow.com/a/38617204/2652918
+@REM @source https://gist.githubusercontent.com/mlocati/fdabcaeb8071d5c75a2d51712db24011/raw/b710612d6320df7e146508094e84b92b34c77d48/win10colors.cmd
+@REM echo [__NUMBER__m__TEXT__[0m
+
+@REM required because variables are set within blocks
+@REM for some reason it is not really working this time
+@REM @link https://www.tenforums.com/general-support/169953-batch-file-issue-related-enabledelayedexpansion.html VIP
+@REM @link https://stackoverflow.com/a/50285862/2652918
+SetLocal EnableDelayedExpansion
+
+@REM move to the directory in which the file currently resides
+@REM @link https://stackoverflow.com/a/5811989/2652918
+@REM @link https://stackoverflow.com/a/636391/2652918
+@REM @link https://stackoverflow.com/a/8797999/2652918
+@REM echo COMMAND: %CD:~0,2% && CD "%~dp0%"
+@REM %CD:~0,2% && CD "%~dp0%" && CD "../.."
+@REM echo COMMAND: %CD:~0,2% && CD "%~dp0%" && ...
+@REM %TAPMEPPE_CD:~0,2% && CD "%TAPMEPPE_CD%"
+set "TAPMEPPE_CD=%cd%"
+set "TAPMEPPE_DIRECTORY=%~dp0%"
+set "TAPMEPPE_SCRIPT=%~n0%~x0"
+%TAPMEPPE_DIRECTORY:~0,2% && CD "%TAPMEPPE_DIRECTORY%"
+
+@REM set the console tile
+title "tapmeppe work - WebReloader"
+
+if "%1" EQU "pull" (
+	echo [36mtapmeppe work - PULL[0m
+	git reset --hard HEAD && git clean -fd && git pull
+	goto :eof
+)
+
+echo [36mtapmeppe work - %1[0m
+npm start %1
